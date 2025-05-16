b99da8002dbabca38d1b94d878d7066b5471f18d
Joan <joanstojkov84@gmail.com>
2025-05-16 14:10:44 +0200

    test declenchement jenkins

--- app-joan.py
+++ app-joan.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-	return "Bonjour, C'est l'application de Joan Stojkov"
+	return "Bonjour, C'est l'application de Joan"
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port =5000)
