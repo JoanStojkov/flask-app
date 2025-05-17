c13f09fbd2dc8a18ed8dd4d0cb27e631a1ceaf54
Joan <joanstojkov84@gmail.com>
2025-05-17 16:30:22 +0200

    Modif de Joan

--- app-gaspard.py
+++ app-gaspard.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Yo, c'est Joan qui a modif"
+    return "Yo, c'est Jojo qui a modif"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
