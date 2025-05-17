e5326bba2617bbd6275b66352eda1efae5062636
module158 <Gaspard2005>
2025-05-17 15:43:24 +0200

    Final test

--- app-gaspard.py
+++ app-gaspard.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello ALED WTF DEVOPS NANI"
+    return "Is batman the best ?"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
